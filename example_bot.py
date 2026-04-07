1c958eaa9328968f69d999f612c5e14792fc8c67
SeanRVACode <133682090+SeanRVACode@users.noreply.github.com>
2026-04-07 10:37:20 -0400

    Reworked example.py to test roll functionality.

--- example_bot.py
+++ example_bot.py
@@ -1,4 +1,5 @@
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 import os
 from typing import Optional, Union
@@ -9,37 +10,36 @@ load_dotenv()
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix="/", intents=intents)
+
+# client = discord.Client(intents=intents)
 
 
-@client.event
-async def on_ready():
-    print(f"We have logged in as {client.user}")
+# @client.event
+# async def on_ready():
+#     print(f"We have logged in as {client.user}")
 
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+# @client.event
+# async def on_message(message):
+#     if message.author == client.user:
+#         return
 
-    if message.content.startswith("$hello"):
-        await message.channel.send("Hello!")
-    if message.content.startswith("/roll"):
-        result, indiv_rolls = roll()
-        await message.channel.send()
-
-
-# Runs the bot using its token.
-client.run(os.getenv("token"))
+#     if message.content.startswith("$hello"):
+#         await message.channel.send("Hello!")
+#     if message.content.startswith("/roll"):
+#         result, indiv_rolls = roll()
+#         await message.channel.send()
 
 
-def roll(player_input: str):
+def roll_results(player_input: str):
     result = 0
     indiv_results = []
+
     count, sides = parse_roll(player_input)
     print(sides)
     if count is None or sides is None:
-        return None
+        return None, None
 
     else:
         while count != 0:
@@ -62,6 +62,19 @@ def parse_roll(dice_str: str):
         return None, None
 
 
-if __name__ == "__main__":
-    result, indiv_rolls = roll("33d6")
-    print(f"The result is: {result}, with individual rolls of {indiv_rolls}")
+@bot.command()
+async def roll(ctx, player_input: str):
+    try:
+        result, indiv_roll = roll_results(player_input)
+        results_dict = {}
+        for _ in indiv_roll:
+            results_dict[_] = indiv_roll.count(_)
+        await ctx.send(f"Total: {result}. Rolled: {results_dict}")
+    except Exception as e:
+        print(e)
+        return None
+
+
+# Runs the bot using its token.
+# client.run(os.getenv("token"))
+bot.run(os.getenv("token"))
